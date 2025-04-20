298b24318628c2d40c48ecfe3cfdf48fce260b53
kmilaaa <camilacristiane37@gmail.com>
2025-04-20 20:08:38 -0300

    base url added

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -14,7 +14,7 @@ describe('Orange HRM Tests', () => {
 
 
   it('Login com sucesso', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userLoginComSucesso.username)
     cy.get(selectorsList.passwordField).type(userData.userLoginComSucesso.password)
     cy.get(selectorsList.loginButton).click()
@@ -22,7 +22,7 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.dashboardGrid)
   })
   it('Login sem sucesso', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userLoginSemSucesso.username)
     cy.get(selectorsList.passwordField).type(userData.userLoginSemSucesso.password)
     cy.get(selectorsList.loginButton).click()
